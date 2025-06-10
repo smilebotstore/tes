7f37fe0efc9229afa25be505a2e8f48a8306e2ed
smilebotstore <ttes61665@gmail.com>
2025-06-10 12:43:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
 const noButton = document.getElementById('noButton');
 if (noButton) {
-    noButton.addEventListener('mouseenter', () => {
+    const moveButton = () => {
         const body = document.body;
         const bodyWidth = body.offsetWidth - noButton.offsetWidth;
         const bodyHeight = body.offsetHeight - noButton.offsetHeight;
         const randX = Math.floor(Math.random() * bodyWidth);
         const randY = Math.floor(Math.random() * bodyHeight);
+        noButton.style.position = 'absolute';  // Penting supaya bisa bergerak
         noButton.style.left = randX + 'px';
         noButton.style.top = randY + 'px';
-    });
+    };
+
+    noButton.addEventListener('mouseenter', moveButton);
+    noButton.addEventListener('click', moveButton);
 }
