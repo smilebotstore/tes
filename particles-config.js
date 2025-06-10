419bce746f002f39bbfd35ea27a2547668407b91
smilebotstore <ttes61665@gmail.com>
2025-06-10 12:10:35 +0800

    Add files via upload

---
+++ particles-config.js
@@ -0,0 +1,25 @@
+
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 30
+    },
+    "shape": {
+      "type": "image",
+      "image": {
+        "src": "https://cdn-icons-png.flaticon.com/512/833/833472.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "size": {
+      "value": 20
+    },
+    "move": {
+      "speed": 2
+    },
+    "opacity": {
+      "value": 0.7
+    }
+  }
+});
